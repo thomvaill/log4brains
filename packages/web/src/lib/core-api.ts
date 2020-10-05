0e430c9f8cafc9a7d76acb9865e1cf6ba835818f
Thomas Vaillant <thomas.vaillant@formetris.com>
2020-10-05 19:23:15 +0200

    feat: LOG4BRAINS_CWD env var

--- packages/web/src/lib/core-api.ts
+++ packages/web/src/lib/core-api.ts
@@ -1,6 +1,6 @@
 import { getInstance } from "@log4brains/core";
 
-const instanceRes = getInstance("../..");
+const instanceRes = getInstance(process.env.LOG4BRAINS_CWD || "."); // TODO: handle errors
 if (instanceRes.isErr()) {
   throw instanceRes.error;
 }
