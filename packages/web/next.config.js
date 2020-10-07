fac0a3b26f13e5f6f267cb6f97d602b71ee36d87
Thomas Vaillant <thomas.vaillant@formetris.com>
2020-10-07 15:10:45 +0200

    chore(web): improve DX with hot reload for depts

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,6 +1,11 @@
-module.exports = {
+// next-transpile-modules is not needed for @log4brains/core,
+// but it brings a better DX because it automatically triggers a Webpack re-build
+// when a change is made in this dependency
+const withTM = require("next-transpile-modules")(["@log4brains/core"]);
+
+module.exports = withTM({
   reactStrictMode: true,
   // basePath: "", // TODO: make this customizable
   target: "serverless",
   poweredByHeader: false
-};
+});
