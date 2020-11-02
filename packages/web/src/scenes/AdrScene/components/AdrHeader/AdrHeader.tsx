40ce6b5a8a1640da1773a09320f8ddb95abe375f
Thomas Vaillant <vaillant.thomas@gmail.com>
2020-11-02 10:21:55 +0100

    feat(web): copy link

--- packages/web/src/scenes/AdrScene/components/AdrHeader/AdrHeader.tsx
+++ packages/web/src/scenes/AdrScene/components/AdrHeader/AdrHeader.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import moment from "moment";
-import { Button, ButtonGroup, Tooltip, Typography } from "@material-ui/core";
+import copyTextToClipboard from "copy-text-to-clipboard";
+import {
+  Button,
+  ButtonGroup,
+  Snackbar,
+  Tooltip,
+  Typography,
+  IconButton
+} from "@material-ui/core";
 import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 import {
   CropFree as CropFreeIcon,
@@ -10,7 +18,8 @@ import {
   Label as LabelIcon,
   Share as ShareIcon,
   GitHub as GitHubIcon,
-  Edit as EditIcon
+  Edit as EditIcon,
+  Close as CloseIcon
 } from "@material-ui/icons";
 import clsx from "clsx";
 import { AdrDto } from "@log4brains/core";
@@ -58,6 +67,13 @@ export type AdrHeaderProps = {
 export function AdrHeader({ className, adr }: AdrHeaderProps) {
   const classes = useStyles();
 
+  const [linkCopiedSnackIsOpened, linkCopiedSnackSetOpened] = React.useState(
+    false
+  );
+  const linkCopiedSnackClose = () => {
+    linkCopiedSnackSetOpened(false);
+  };
+
   const decidersIcon =
     adr.deciders.length > 1 ? (
       <PeopleIcon className={classes.icon} fontSize="inherit" />
@@ -66,75 +82,102 @@ export function AdrHeader({ className, adr }: AdrHeaderProps) {
     );
 
   return (
-    <div className={clsx(className, classes.root)}>
-      <div>
-        <div className={classes.inlineInfo}>
-          {adr.package ? (
-            <Typography variant="body2" title="Package">
-              <CropFreeIcon className={classes.icon} fontSize="inherit" />{" "}
-              {adr.package}
+    <>
+      <div className={clsx(className, classes.root)}>
+        <div>
+          <div className={classes.inlineInfo}>
+            {adr.package ? (
+              <Typography variant="body2" title="Package">
+                <CropFreeIcon className={classes.icon} fontSize="inherit" />{" "}
+                {adr.package}
+              </Typography>
+            ) : null}
+
+            <Typography
+              variant="body2"
+              title={adr.publicationDate ? "Publication date" : "Creation date"}
+            >
+              <EventIcon className={classes.icon} fontSize="inherit" />{" "}
+              {moment(adr.publicationDate || adr.creationDate).format("ll")}
             </Typography>
-          ) : null}
+
+            <div title="Status">
+              <AdrStatusChip status={adr.status} />
+            </div>
+          </div>
 
           <Typography
             variant="body2"
-            title={adr.publicationDate ? "Publication date" : "Creation date"}
+            title={
+              adr.deciders.length > 0
+                ? `Decider${adr.deciders.length > 1 ? "s" : ""}`
+                : "Author"
+            }
           >
-            <EventIcon className={classes.icon} fontSize="inherit" />{" "}
-            {moment(adr.publicationDate || adr.creationDate).format("ll")}
+            {decidersIcon}{" "}
+            {adr.deciders.length > 1
+              ? adr.deciders.join(", ")
+              : adr.lastEditAuthor}
           </Typography>
 
-          <div title="Status">
-            <AdrStatusChip status={adr.status} />
+          {adr.tags.length > 0 ? (
+            <Typography variant="body2" title="Tags">
+              <LabelIcon className={classes.icon} fontSize="inherit" />{" "}
+              <span className={classes.tags}>
+                {adr.tags.map((tag) => `#${tag}`).join(" ")}
+              </span>
+            </Typography>
+          ) : null}
+        </div>
+        <div>
+          <div>
+            <ButtonGroup size="medium">
+              <Tooltip title="Copy link">
+                <Button
+                  onClick={() => {
+                    copyTextToClipboard(
+                      window.location.href.replace(window.location.hash, "")
+                    );
+                    linkCopiedSnackSetOpened(true);
+                  }}
+                >
+                  <ShareIcon fontSize="small" />
+                </Button>
+              </Tooltip>
+
+              <Tooltip title="View/edit on Github">
+                <Button>
+                  <GitHubIcon fontSize="small" />
+                </Button>
+              </Tooltip>
+
+              <Tooltip title="Edit locally">
+                <Button color="secondary" onClick={() => editLocally(adr.slug)}>
+                  <EditIcon fontSize="small" />
+                </Button>
+              </Tooltip>
+            </ButtonGroup>
           </div>
         </div>
-
-        <Typography
-          variant="body2"
-          title={
-            adr.deciders.length > 0
-              ? `Decider${adr.deciders.length > 1 ? "s" : ""}`
-              : "Author"
-          }
-        >
-          {decidersIcon}{" "}
-          {adr.deciders.length > 1
-            ? adr.deciders.join(", ")
-            : adr.lastEditAuthor}
-        </Typography>
-
-        {adr.tags.length > 0 ? (
-          <Typography variant="body2" title="Tags">
-            <LabelIcon className={classes.icon} fontSize="inherit" />{" "}
-            <span className={classes.tags}>
-              {adr.tags.map((tag) => `#${tag}`).join(" ")}
-            </span>
-          </Typography>
-        ) : null}
       </div>
-      <div>
-        <div>
-          <ButtonGroup size="medium">
-            <Tooltip title="Share">
-              <Button>
-                <ShareIcon fontSize="small" />
-              </Button>
-            </Tooltip>
-
-            <Tooltip title="Edit on Github">
-              <Button>
-                <GitHubIcon fontSize="small" />
-              </Button>
-            </Tooltip>
-
-            <Tooltip title="Edit locally">
-              <Button color="secondary" onClick={() => editLocally(adr.slug)}>
-                <EditIcon fontSize="small" />
-              </Button>
-            </Tooltip>
-          </ButtonGroup>
-        </div>
-      </div>
-    </div>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        open={linkCopiedSnackIsOpened}
+        onClose={linkCopiedSnackClose}
+        autoHideDuration={6000}
+        message="Link copied to clipboard"
+        action={
+          <IconButton
+            size="small"
+            aria-label="close"
+            color="inherit"
+            onClick={linkCopiedSnackClose}
+            title="Close"
+          >
+            <CloseIcon fontSize="small" />
+          </IconButton>
+        }
+      />
+    </>
   );
 }
